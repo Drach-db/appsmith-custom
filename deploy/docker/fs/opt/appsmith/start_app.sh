0819313276127a07034c96e2c5ea117db763f519
Dabiil <drach.db1@gmail.com>
2025-06-27 18:47:01 +0800

    Fix: safe mv only if archive exists

--- deploy/docker/fs/opt/appsmith/start_app.sh
+++ deploy/docker/fs/opt/appsmith/start_app.sh
@@ -3,15 +3,20 @@
 install_dir="$1"
 
 cd "$install_dir" \
-&& echo "Start application..." \
-&& mv "$install_dir/stacks/data/backup/appsmith-data.archive" "$install_dir/stacks/data/restore" \
-&& docker-compose up -d
+&& echo "Start application..."
+
+# Безопасное перемещение архива, только если он существует
+if [[ -f "$install_dir/stacks/data/backup/appsmith-data.archive" ]]; then
+  mv "$install_dir/stacks/data/backup/appsmith-data.archive" "$install_dir/stacks/data/restore"
+fi
+
+docker-compose up -d
 
 wait_for_containers_start() {
     local timeout=$1
     while [[ $timeout -gt 0 ]]; do
         status_code="$(curl -s -o /dev/null -w "%{http_code}" http://localhost/api/v1 || true)"
-        if [[ status_code -eq 401 ]]; then
+        if [[ $status_code -eq 401 ]]; then
             break
         else
             echo -ne "Waiting for all containers to start. This check will timeout in $timeout seconds...\r\c"
@@ -22,5 +27,6 @@ wait_for_containers_start() {
 }
 
 wait_for_containers_start 180
-# Force import DB to ignore confirm step
+
+# Импорт БД
 docker-compose exec -T appsmith appsmithctl import_db -f
